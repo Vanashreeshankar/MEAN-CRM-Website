f237b548b0aecff74f2cc8bd7ef4a5c1022d3246
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2024-08-31 00:07:46 +0530

    Update email.helper.js

--- backend/functions/email.helper.js
+++ backend/functions/email.helper.js
@@ -1,76 +1,89 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: "smtp.gmail.com",
+  host: "smtp.ethereal.email",
+  port: 587,
   auth: {
-    user: "crm.company.in@gmail.com",
-    pass: "ncleywbpnfoipryi"
+    user: "abe.kohler59@ethereal.email",
+    pass: "8Bft1DC6qX7319GZ1f",
   },
-  logger: true, // Enable logging to console
-  debug: true, // Include SMTP traffic in the logs
-  tls: {
-    rejectUnauthorized: false
-  },
-  port: 465,
-  secure: true,
 });
 
-const send = async (info) => {
-  try {
-    // send mail with defined transport object
-    let result = await transporter.sendMail(info);
+const send = (info) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // send mail with defined transport object
+      let result = await transporter.sendMail(info);
 
-    console.log("Message sent: %s", result.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      console.log("Message sent: %s", result.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-    return result;
-  } catch (error) {
-    console.log("Error in sending email:", error);
-    throw error;
-  }
+      resolve(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
 
-const emailProcessor = async ({ email, pin, type, verificationLink = "https://mean-crm-frontend.vercel.app/resetpassword" }) => {
-  console.log('Email:', email, pin, type, verificationLink);
+const emailProcessor = ({ email, pin, type, verificationLink = "" }) => {
   let info = "";
   switch (type) {
     case "request-new-password":
       info = {
-        from: '"CMR Company" <crm.company.in@gmail.com>', // sender address
+        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
         to: email, // list of receivers
-        subject: "Password reset Pin", // Subject line
-        text: "Here is your password reset pin " + pin + " This pin will expire in 1 day", // plain text body
+        subject: "Password rest Pin", // Subject line
+        text:
+          "Here is your password rest pin" +
+          pin +
+          " This pin will expires in 1day", // plain text body
         html: `<b>Hello </b>
       Here is your pin 
-      <b>${pin}</b>
-      This pin will expire in 1 day
-      <p href="${verificationLink}">Reset Password</P>
+      <b>${pin} </b>
+      This pin will expires in 1day
       <p></p>`, // html body
       };
 
-      await send(info);
+      send(info);
       break;
 
     case "update-password-success":
       info = {
-        from: '"CMR Company" <crm.company.in@gmail.com>', // sender address
+        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
         to: email, // list of receivers
         subject: "Password updated", // Subject line
-        text: "Your new password has been updated", // plain text body
+        text: "Your new password has been update", // plain text body
         html: `<b>Hello </b>
-        <p>Your new password has been updated</p>`, // html body
+       
+      <p>Your new password has been update</p>`, // html body
       };
-      await send(info);
+
+      send(info);
       break;
+
+    case "new-user-confirmation-required":
+      info = {
+        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
+        to: email, // list of receivers
+        subject: "Please verify your new user", // Subject line
+        text:
+          "Please follow the link to very your account before you can login", // plain text body
+        html: `<b>Hello </b>
+        <p>Please follow the link to very your account before you can login</p>
+        <p>${verificationLink}</P>
+        `, // html body
+      };
+
+      send(info);
+      break;
+
     default:
-      console.log("Invalid email type provided");
       break;
   }
 };
 
-module.exports = emailProcessor;
+module.exports = { emailProcessor };
