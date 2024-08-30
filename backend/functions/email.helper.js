6135f0b2e12a56d205c8187a28bc9f354295481b
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2024-08-30 20:30:14 +0530

    Update email.helper.js

--- backend/functions/email.helper.js
+++ backend/functions/email.helper.js
@@ -16,28 +16,27 @@ const transporter = nodemailer.createTransport({
   secure: true,
 });
 
-const send = (info) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // send mail with defined transport object
-      let result = await transporter.sendMail(info);
+const send = async (info) => {
+  try {
+    // send mail with defined transport object
+    let result = await transporter.sendMail(info);
 
-      console.log("Message sent: %s", result.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", result.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-      resolve(result);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    return result;
+  } catch (error) {
+    console.log("Error in sending email:", error);
+    throw error;
+  }
 };
 
-const emailProcessor = ({ email, pin, type, verificationLink = "https://mean-crm-frontend.vercel.app/resetpassword" }) => {
-  console.log('Email:', email, pin, type, verificationLink)
+const emailProcessor = async ({ email, pin, type, verificationLink = "https://mean-crm-frontend.vercel.app/resetpassword" }) => {
+  console.log('Email:', email, pin, type, verificationLink);
   let info = "";
   switch (type) {
     case "request-new-password":
@@ -54,7 +53,7 @@ const emailProcessor = ({ email, pin, type, verificationLink = "https://mean-crm
       <p></p>`, // html body
       };
 
-      send(info);
+      await send(info);
       break;
 
     case "update-password-success":
@@ -66,12 +65,12 @@ const emailProcessor = ({ email, pin, type, verificationLink = "https://mean-crm
         html: `<b>Hello </b>
         <p>Your new password has been updated</p>`, // html body
       };
-      send(info);
+      await send(info);
       break;
     default:
+      console.log("Invalid email type provided");
       break;
   }
 };
 
-
-module.exports =  emailProcessor ;
+module.exports = emailProcessor;
