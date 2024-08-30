b72dc32dd74e5a21f997475b2dc59a427531556e
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2024-08-30 15:34:03 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,20 +3,7 @@ require('dotenv').config();
 
 const connectionString =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9ygt.mongodb.net/crm_db`
 
-
-/*mongoose.connect(connectionString, (error) => {
-    if(!error){
-        console.log('Connection Successful')
-    }else{
-        console.log('Connection Unsuccessful' + " " + error)
-    }
-});*/
-
-mongoose.connect(connectionString,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-)
+mongoose.connect(connectionString)
 .then(() => console.log('DB Connection Successfull'))
 .catch((err) => {
     console.error(err);
