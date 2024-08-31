c6e6b5b7420161504bb324b569bba1f68f0aaeb4
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2024-08-31 17:48:33 +0530

    Update email.helper.js

--- backend/functions/email.helper.js
+++ backend/functions/email.helper.js
@@ -1,50 +1,50 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
+ 
+  service:'gmail',
   auth: {
-    user: "abe.kohler59@ethereal.email",
-    pass: "8Bft1DC6qX7319GZ1f",
-  },
+    user: process.env.EMAIL_USER, 
+   pass: process.env.EMAIL_PASS
+ },
+ 
+ tls: {
+    rejectUnauthorized: false
+  }, 
+port: 465,
+secure: false
 });
 
 const send = (info) => {
   return new Promise(async (resolve, reject) => {
     try {
-      // send mail with defined transport object
       let result = await transporter.sendMail(info);
-
-      console.log("Message sent: %s", result.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(result));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-      resolve(result);
+  console.log("Message sent: %s", result.messageId);
+  console.log("Full result:", result);
+  resolve(result);
     } catch (error) {
+      console.log('error sending mail:',error)
+     console.error('TLS error:', error.message)
       console.log(error);
     }
   });
 };
 
-const emailProcessor = ({ email, pin, type, verificationLink = "" }) => {
+const emailProcessor = ({ email, pin, type, verificationLink = "https://mean-crm-frontend.vercel.app/resetpassword" }) => {
+  console.log('Email:', email, pin, type, verificationLink)
   let info = "";
   switch (type) {
     case "request-new-password":
       info = {
-        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
+        from: '"CMR Company" <crm.company.in@gmail.com>', // sender address
         to: email, // list of receivers
-        subject: "Password rest Pin", // Subject line
-        text:
-          "Here is your password rest pin" +
-          pin +
-          " This pin will expires in 1day", // plain text body
+        subject: "Password reset Pin", // Subject line
+        text: "Here is your password reset pin " + pin + " This pin will expire in 1 day", // plain text body
         html: `<b>Hello </b>
       Here is your pin 
-      <b>${pin} </b>
-      This pin will expires in 1day
+      <b>${pin}</b>
+      This pin will expire in 1 day
+      <p href="${verificationLink}">Reset Password</P>
       <p></p>`, // html body
       };
 
@@ -53,37 +53,18 @@ const emailProcessor = ({ email, pin, type, verificationLink = "" }) => {
 
     case "update-password-success":
       info = {
-        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
+        from: '"CMR Company" <crm.company.in@gmail.com>', // sender address
         to: email, // list of receivers
         subject: "Password updated", // Subject line
-        text: "Your new password has been update", // plain text body
+        text: "Your new password has been updated", // plain text body
         html: `<b>Hello </b>
-       
-      <p>Your new password has been update</p>`, // html body
+        <p>Your new password has been updated</p>`, // html body
       };
-
       send(info);
       break;
-
-    case "new-user-confirmation-required":
-      info = {
-        from: '"CMR Company" <abe.kohler59@ethereal.email>', // sender address
-        to: email, // list of receivers
-        subject: "Please verify your new user", // Subject line
-        text:
-          "Please follow the link to very your account before you can login", // plain text body
-        html: `<b>Hello </b>
-        <p>Please follow the link to very your account before you can login</p>
-        <p>${verificationLink}</P>
-        `, // html body
-      };
-
-      send(info);
-      break;
-
     default:
       break;
   }
 };
 
-module.exports = { emailProcessor };
+module.exports =  emailProcessor ;
