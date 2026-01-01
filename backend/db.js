b2f689316da4dd42368e672008ae906ef8afa55b
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2026-01-01 21:27:13 +0530

    Update db.js
    Fix MongoDB connection for Vercel serverless

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,28 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require('dotenv').config(); //not need in vercel deployment
 
-const connectionString =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9ygt.mongodb.net/crm_db`
+let cached = global.mongoose;
 
-mongoose.connect(connectionString)
-.then(() => console.log('DB Connection Successfull'))
-.catch((err) => {
-    console.error(err);
-});
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+   const connectionString =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9ygt.mongodb.net/crm_db`
+
+    cached.promise = mongoose.connect(connectionString, {
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 module.exports = mongoose;
- 
+
