a2379f66d7ee87074d91357e5206e043aa2ea973
Vanashreeshankar <57714270+Vanashreeshankar@users.noreply.github.com>
2026-01-02 20:03:32 +0530

    Update db.js
    Undo the changes

--- backend/db.js
+++ backend/db.js
@@ -1,28 +1,14 @@
-const mongoose = require("mongoose");
-require('dotenv').config(); //not need in vercel deployment
 
-let cached = global.mongoose;
+const mongoose = require('mongoose'); 
+require('dotenv').config(); 
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-   const connectionString =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9ygt.mongodb.net/crm_db`
-
-    cached.promise = mongoose.connect(connectionString, {
-      bufferCommands: false,
-    }).then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9ygt.mongodb.net/crm_db` 
+mongoose.connect(connectionString) 
+.then(() => console.log('DB Connection Successfull')) 
+.catch((err) => 
+{ 
+console.error(err); 
+}); 
 
 module.exports = mongoose;
 
